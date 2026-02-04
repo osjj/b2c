107d56f9bedd820b17cfec796e6a4646dfc2c6bc
trvrntja <trvrntja744@gmail.com>
2026-02-04 18:30:22 +0800

    update

--- scripts/list-solutions.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const p = new PrismaClient()
-async function main() {
-  const s = await p.solution.findMany({ select: { slug: true, usageScenes: true } })
-  console.log(JSON.stringify(s, null, 2))
-}
-main().finally(() => p.$disconnect())
