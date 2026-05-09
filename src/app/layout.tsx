1245d82e5a1b0c46a2dad0eca00c9ce14bd8dcba
zgnadklhxr <zgnadklhxr438@gmail.com>
2026-05-09 23:57:34 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { getSiteUrl } from "@/lib/site-url";
 import "./globals.css";
 
 const baseUrl = getSiteUrl()
+const META_PIXEL_ID = "1405506711622457"
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -73,6 +75,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1" alt="" />`,
+          }}
+        />
         {children}
       </body>
     </html>
